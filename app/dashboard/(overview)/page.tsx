40a38d2caca26b623244e5cabfcfb2d562d58bb5
aspandyar <aspandyart@gmail.com>
2026-05-27 03:12:09 +0500

    add metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -5,7 +5,12 @@ import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { fetchCardData } from '@/app/lib/data';
 import { CardSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
- 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
+
 export default async function Page() {
   return (
     <main>
--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,7 +6,12 @@ import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
- 
+import { Metadata } from 'next'; 
+
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
+
 export default async function Page(props: {
   searchParams?: Promise<{
     query?: string;
@@ -19,7 +24,6 @@ export default async function Page(props: {
     const totalPages = await fetchInvoicesPages(query);
 
     return (
-
     <div className="w-full">
         <div className="flex w-full items-center justify-between">
             <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
